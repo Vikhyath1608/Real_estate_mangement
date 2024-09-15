eb6a6615c5c9b7f02ea711185b69a4cbc1aa8861
Vikhyath <vikhyathraims0901@gmail.com>
2024-09-15 14:28:04 +0530

    first commit

---
+++ src/main/java/com/aiet/real_estate_management_system/controller/PropertyController.java
@@ -0,0 +1,5 @@
+package com.aiet.real_estate_management_system.controller;
+
+public class PropertyController {
+
+}
---
+++ src/main/java/com/aiet/real_estate_management_system/mapper/PropertyMapper.java
@@ -0,0 +1,5 @@
+package com.aiet.real_estate_management_system.mapper;
+
+public class PropertyMapper {
+
+}
